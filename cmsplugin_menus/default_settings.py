6054cbea392766ca843ff1ba8811de2715c855b7
powderflask <Joseph@Kaya.local>
2010-11-03 11:02:30 -0700

    Fixed bug when CMS_PLACEHOLDER_CONF was unspecified.

--- cmsplugin_menus/default_settings.py
+++ cmsplugin_menus/default_settings.py
@@ -1,6 +1,5 @@
 """
-Default menu settings, are applied if there isn't value defined in project
-settings. 
+Default menu settings, are applied only if there isn't value defined in project settings. 
 All available settings are listed here - override them in your project settings.
 """
 import constants
@@ -13,3 +12,5 @@ CMSPLUGIN_MENUS_PLACEHOLDER_CONF = {
                 'plugins': ('LinkPlugin', 'SnippetPlugin'),
                 'name': gettext("links")
 }}
+
+CMS_PLACEHOLDER_CONF = {}  # this is an optional CMS setting, but we need it to exist
